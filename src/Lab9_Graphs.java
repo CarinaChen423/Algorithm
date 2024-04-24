31b5cd0f860cedf7dd22a5526a981295d7ca866d
CarinaChen423 <chaselc011@gmail.com>
2024-04-24 18:55:26 +0200

    Lab9

--- src/Lab9_Graphs.java
+++ src/Lab9_Graphs.java
@@ -1,61 +1,163 @@
-import com.sun.source.tree.MemberSelectTree;
-
-import java.io.*;
-import java.lang.*;
 import java.util.*;
 
-public class Lab9_Graphs {
-    private static final int V = 5;
-    int minKey(int key[], Boolean mstSet[]){
-        int min = Integer.MAX_VALUE, min_index = -1;
-        for (int v = 0; v < V; v++){
-            if(mstSet[v] == false && key[v] < min){
-                min = key[v];
-                min_index = v;
+class Edge implements Comparable<Edge> {
+    int src, dest, weight;
+
+    public Edge(int src, int dest, int weight) {
+        this.src = src;
+        this.dest = dest;
+        this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(Edge compareEdge) {
+        return this.weight - compareEdge.weight;
+    }
+}
+
+class Subset {
+    int parent, rank;
+}
+
+class Graph {
+    int vertices;
+    LinkedList<Edge>[] adjacencylist;
+    ArrayList<Edge> edges;
+
+    Graph(int vertices) {
+        this.vertices = vertices;
+        adjacencylist = new LinkedList[vertices];
+        edges = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adjacencylist[i] = new LinkedList<>();
+        }
+    }
+
+    void addEdge(int src, int dest, int weight) {
+        Edge edge = new Edge(src, dest, weight);
+        adjacencylist[src].add(edge);
+        edges.add(edge); // For Kruskal's use
+    }
+
+    // Kruskal's algorithm
+    ArrayList<Edge> kruskalMST() {
+        PriorityQueue<Edge> pq = new PriorityQueue<>(edges.size(), Comparator.comparingInt(o -> o.weight));
+        pq.addAll(edges);
+
+        Subset[] subsets = new Subset[vertices];
+        for (int i = 0; i < vertices; ++i) {
+            subsets[i] = new Subset();
+            subsets[i].parent = i;
+            subsets[i].rank = 0;
+        }
+
+        ArrayList<Edge> result = new ArrayList<>();
+        while (result.size() < vertices - 1) {
+            Edge edge = pq.remove();
+            int x = find(subsets, edge.src);
+            int y = find(subsets, edge.dest);
+
+            if (x != y) {
+                result.add(edge);
+                union(subsets, x, y);
             }
         }
-        return min_index;
+        return result;
     }
 
-    void printMST(int parent[], int graph[][]){
-        System.out.println("Edge \tWeight");
-        for(int i=1; i < V; i++){
-            System.out.println(parent[i] + " - " + i + "\t" + graph[i][parent[i]]);
-        }
-    }
+    // Prim's algorithm
+    ArrayList<Edge> primMST() {
+        boolean[] mstSet = new boolean[vertices];
+        Edge[] resultSet = new Edge[vertices];
+        int[] key = new int[vertices];
 
-    void primMST(int graph[][]){
-        int parent[] = new int[V];
-        int key[] = new int[V];
-        Boolean mstSet[] = new Boolean[V];
-        for(int i = 0; i < V; i++){
+        for (int i = 0; i < vertices; i++) {
             key[i] = Integer.MAX_VALUE;
-            mstSet[i] = false;
         }
-        key[0] = 0;
-        parent[0] = -1;
 
-        for(int count = 0; count < V-1; count++){
-            int u = minKey(key,mstSet);
+        key[0] = 0;
+        resultSet[0] = new Edge(-1, 0, 0);
+
+        for (int i = 0; i < vertices - 1; i++) {
+            int minIndex = -1;
+            int minKey = Integer.MAX_VALUE;
+            for (int v = 0; v < vertices; v++) {
+                if (!mstSet[v] && key[v] < minKey) {
+                    minKey = key[v];
+                    minIndex = v;
+                }
+            }
+
+            int u = minIndex;
             mstSet[u] = true;
-            for(int v = 0; v < V; v++){
-                if(graph[u][v] !=0 && mstSet[v] == false && graph[u][v] < key[v]){
-                    parent[v] = u;
-                    key[v] = graph[u][v];
+
+            for (Edge neighbor : adjacencylist[u]) {
+                if (!mstSet[neighbor.dest] && neighbor.weight < key[neighbor.dest]) {
+                    resultSet[neighbor.dest] = neighbor;
+                    key[neighbor.dest] = neighbor.weight;
                 }
             }
         }
-        printMST(parent, graph);
+
+        ArrayList<Edge> result = new ArrayList<>();
+        for (int v = 1; v < vertices; v++) {
+            result.add(resultSet[v]);
+        }
+        return result;
     }
 
+    // A utility function to find set of an element i (uses path compression)
+    int find(Subset subsets[], int i) {
+        if (subsets[i].parent != i)
+            subsets[i].parent = find(subsets, subsets[i].parent);
+        return subsets[i].parent;
+    }
+
+    // A function that does union of two sets of x and y (uses union by rank)
+    void union(Subset subsets[], int x, int y) {
+        int xroot = find(subsets, x);
+        int yroot = find(subsets, y);
+
+        if (subsets[xroot].rank < subsets[yroot].rank)
+            subsets[xroot].parent = yroot;
+        else if (subsets[xroot].rank > subsets[yroot].rank)
+            subsets[yroot].parent = xroot;
+        else {
+            subsets[yroot].parent = xroot;
+            subsets[xroot].rank++;
+        }
+    }
+}
+
+public class Lab9_Graphs {
     public static void main(String[] args) {
-        Lab9_Graphs t = new Lab9_Graphs();
-        int graph[][] = new int [][] {
-                { 0, 2, 0, 6, 0 },
-                { 2, 0, 3, 8, 5 },
-                { 0, 3, 0, 0, 7 },
-                { 6, 8, 0, 0, 9 },
-                { 0, 5, 7, 9, 0 } };
-        t.primMST(graph);
+        int vertices = 8;
+        Graph graph = new Graph(vertices);
+        graph.addEdge(0, 1, 4); // A-B
+        graph.addEdge(0, 2, 3); // A-C
+        graph.addEdge(0, 6, 6); // A-G
+        graph.addEdge(1, 2, 3); // B-C
+        graph.addEdge(1, 3, 3); // B-D
+        graph.addEdge(2, 3, 6); // C-D
+        graph.addEdge(2, 4, 5); // C-E
+        graph.addEdge(3, 4, 3); // D-E
+        graph.addEdge(3, 5, 6); // D-F
+        graph.addEdge(4, 5, 3); // E-F
+        graph.addEdge(4, 6, 4); // E-G
+        graph.addEdge(5, 7, 2); // F-H
+        graph.addEdge(6, 7, 7); // G-H
+
+        ArrayList<Edge> primMST = graph.primMST();
+        ArrayList<Edge> kruskalMST = graph.kruskalMST();
+
+        System.out.println("Prim's MST:");
+        for (Edge edge : primMST) {
+            System.out.println((char)('A' + edge.src) + " - " + (char)('A' + edge.dest) + ": " + edge.weight);
+        }
+
+        System.out.println("Kruskal's MST:");
+        for (Edge edge : kruskalMST) {
+            System.out.println((char)('A' + edge.src) + " - " + (char)('A' + edge.dest) + ": " + edge.weight);
+        }
     }
 }
